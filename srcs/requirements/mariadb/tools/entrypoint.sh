7a6b8de3928cb0a031c1427f82f27bc3447fb675
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-08-25 18:30:34 +0000

    test : update pass var

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# Start MariaDB service
 
+# MariaDB Configuration Variables
+DB_NAME="maraidb_name"
+DB_USER="ali"
+DB_PASS="123"  # Ensure this matches the password in WordPress configuration
+DB_ROOT_PASS="123"
+
+# Display the MySQL variables
 echo "DB_ROOT_PASS: $DB_ROOT_PASS"
 echo "DB_NAME: $DB_NAME"
 echo "DB_USER: $DB_USER"
 echo "DB_PASS: $DB_PASS"
 
+# Start MariaDB service
 service mariadb start
 sleep 5
 
@@ -20,4 +27,4 @@ mysql -u root -p"$DB_ROOT_PASS" -e "FLUSH PRIVILEGES;"
 service mariadb stop
 
 # Start MySQL server daemon with specified configurations
-mysqld_safe --port=3306 --bind-address=0.0.0.0 
+mysqld_safe --port=3306 --bind-address=0.0.0.0

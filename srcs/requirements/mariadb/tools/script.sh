cd0149411ce470b0a41f84cfa886d83c474af28f
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-10-05 21:48:13 +0100

    clean up

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-# 1. Start MariaDB service
+
 service mariadb start
 
-# Wait for 5 seconds to ensure MariaDB has started properly
+
 sleep 5
 
-# 2. Configure MariaDB
-# Set the root password
+
+
 mysqladmin -u root password "$MARIADB_ROOT_PASSWORD"
 
-# Create the specified database if it does not already exist
+
 mysql -u root -p"$MARIADB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;"
 
-# Create a new user with the specified password if it does not already exist
+
 mysql -u root -p"$MARIADB_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';"
 
-# Grant all privileges on the specified database to the new user
+
 mysql -u root -p"$MARIADB_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%';"
 
-# Apply the privilege changes
+
 mysql -u root -p"$MARIADB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-# 3. Stop the MariaDB service
+
 service mariadb stop
 
-# Start the MariaDB server in safe mode, allowing it to accept connections from any IP address
-# Bind address is set to 0.0.0.0 to accept connections from any network interface
+
 mysqld_safe --port=3306 --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
-# Wait for 10 seconds to allow other services or dependencies to start up.
+
 sleep 10
 
-# Start PHP-FPM service.
+
 service php7.4-fpm start
 
-# Prepare the WordPress installation directory.
+
 mkdir -p /var/www/html
 cd /var/www/html
 
-# Download the latest version of WordPress.
+
 wp core download --allow-root
 
-# Create the WordPress configuration file using the specified database credentials.
+
 wp config create --allow-root --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=$DB_HOST
 
-# Install WordPress with the provided site information and admin credentials.
+
 wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
-# Create a new WordPress user with the specified details and role.
+
 wp user create --allow-root $WP_USER_LOGIN $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=$WP_USER_ROLE
 
-# Stop the PHP-FPM service.
+
 service php7.4-fpm stop
 
-# Start PHP-FPM in the foreground to keep the container running.
+
 php-fpm7.4 -F
